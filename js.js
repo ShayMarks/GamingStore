5946970d6f9351f5025c096d16fc9eb23ff02cdd
shay <nrexhd@gmail.com>
2023-07-05 16:13:41 +0300

    GamingStore

---
+++ js.js
@@ -0,0 +1,391 @@
+// Define the products
+// C1 Keyboard
+let p1={Pid: 100, Pname: "HP K500F", price: 50, image:"products/C1_Keyboard/HPkey.png", categoryId: 1,
+PNote: "Dynamic RGB lighting and programmable macros for enhanced gaming customization"
+, Prate: '★★★☆☆'};
+let p2={Pid: 200, Pname: "CROSAIR K55 RGB PRO", price: 55, image: "products/C1_Keyboard/k55protop.png", categoryId: 1, 
+PNote: "Dynamic RGB lighting and programmable macros for enhanced gaming customization"
+, Prate: '★★★★☆'};
+let p3={Pid: 300, Pname: "RAZER blackwidow chroma", price: 75, image: "products/C1_Keyboard/razerblackwidowchroma.png", categoryId: 1, 
+PNote: "Premium build quality, customizable RGB lighting, and responsive mechanical switches for optimal gaming performance"
+, Prate: "★★★★☆"};
+let p4={Pid: 400, Pname: "HyperX Alloy Origins Core", price: 80, image: "products/C1_Keyboard/HYPERX.png", categoryId: 1, 
+PNote: "Compact design, durable build quality, and responsive mechanical switches for a reliable and enjoyable gaming experience"
+, Prate: "★★★★☆"};
+let p37={Pid: 3700, Pname: "ZIYOU LANG T6", price: 40, image:"products/C1_Keyboard/ZiyouLangT60Id17.png", categoryId: 1,
+PNote: "high-quality materials, customizable features provide a comfortable"
+, Prate: "★★☆☆☆"};
+let p38={Pid: 3800, Pname: "alienware aw51k", price: 110, image: "products/C1_Keyboard/alienwareAW510K.png", categoryId: 1, 
+PNote: "slim design, responsive low-profile mechanical switches, RGB lighting deliver a premium gaming experience with sleek aesthetic"
+, Prate: "★★★★★"};
+let p39={Pid: 3900, Pname: "Logitech Pro X", price: 120, image: "products/C1_Keyboard/LogitechProX.png", categoryId: 1, 
+PNote: "durable build, customizable RGB lighting, and advanced mechanical switches"
+, Prate: "★★★★★"};
+let p40={Pid: 4000, Pname: "Freestyle Edge RGB", price: 215, image: "products/C1_Keyboard/freestyleEdgeRGB.png", categoryId: 1, 
+PNote: "Split design, ergonomic layout and customizable RGB lighting enable personal comfort and efficiency during gaming sessions"
+, Prate: "★★★★★"};
+
+// C2 Mouse
+let p5={Pid: 500, Pname: "HyperX Pulsefire", price: 25, image: "products/C2_Mouse/RE2Pm4N.png", categoryId: 2, 
+PNote: "Reliable and comfortable with responsive buttons and an accurate sensor"
+, Prate: "★★☆☆☆"};
+let p6={Pid: 600, Pname: "AsusROGGladiusIII", price: 50, image: "products/C2_Mouse/AsusROGGladiusIII.png", categoryId: 2, 
+PNote: "Ergonomic design, customizable buttons, and high-precision sensor for accurate and comfortable gaming control, offering a premium gaming experience"
+, Prate: "★★★☆☆"};
+let p7={Pid: 700, Pname: "RAZER BASILISK V3 PRO", price: 75, image: "products/C2_Mouse/basiliskv3.png", categoryId: 2,
+PNote: "Wireless versatility, customizable buttons, and high-precision sensor for enhanced gaming control and convenience."
+, Prate: "★★★★☆"};
+let p8={Pid: 800, Pname: "Logitech G502 Lightspeed", price: 80, image: "products/C2_Mouse/LOGITECHG502.png", categoryId: 2,
+PNote: "Wireless freedom, adjustable weight system, and high-performance sensor for precise and responsive gaming movements"
+, Prate: "★★★★★"};
+
+// C3 Headphones
+let p9={Pid: 900, Pname: "RAZER kraken X", price: 55, image: "products/C3_Headphones/razerkrakenx.png", categoryId: 3, 
+PNote: "The immersive sound and light design that enhances your gaming experience"
+, Prate: "★★☆☆☆"};
+let p10={Pid: 1000, Pname: "HyperX Cloud II", price: 90, image: "products/C3_Headphones/HyperXCloudII.png", categoryId: 3, 
+PNote: "exceptional comfort, superior sound quality, and virtual 7.1"
+, Prate: "★★★☆☆"};
+let p11={Pid: 1100, Pname: "Steelseries Arctis 7", price: 150, image: "products/C3_Headphones/SteelSeriesArcti7.png", categoryId: 3, 
+PNote: "Wireless comfort, exceptional comfort and high quality audio for an immersive gaming experience"
+, Prate: "★★★★☆"};
+let p12={Pid: 1200, Pname: "Asus ROG THETA 7.1", price: 300, image: "products/C3_Headphones/rogt.png", categoryId: 3, 
+PNote: "Premium build quality, immersive 7.1 surround sound and customizable lighting effects make it a top-notch gaming headset"
+, Prate: "★★★★★"};
+
+// C4 Chairs
+let p13={Pid: 1300, Pname: "HyperX Alloy Origins Core", price: 130, image: "products/C4_Chairs/scorpion.png", categoryId: 4, 
+PNote: "The unique design inspired by the scorpion, the ergonomic features and the components that offer a particularly comfortable and immersive gaming experience"
+, Prate: "★★★★☆"};
+let p14={Pid: 1400, Pname: "DRAGON Gaming Chair Olympus", price: 200, image: "products/C4_Chairs/dragon.png", categoryId: 4, 
+PNote: "ergonomic design, premium build quality, and adjustable features provide optimal comfort and support for long gaming sessions"
+, Prate: "★★★★☆"};
+let p15={Pid: 1500, Pname: "COUGAR RANGER Sofa", price: 250, image: "products/C4_Chairs/COUGARRANGE.png", categoryId: 4, 
+PNote: "Combines luxurious comfort, ergonomic design and built-in features"
+, Prate: "★★★★★"};
+let p16={Pid: 1600, Pname: "RAZER ISKUR", price: 450, image: "products/C4_Chairs/RAZERISKUR.png", categoryId: 4,
+PNote: "whose ergonomic design, lumbar support system ensure maximum comfort during gaming marathons"
+, Prate: "★★★★★"};
+
+// C5 Consoles
+let p17={Pid: 1700, Pname: "Sony PlayStation 5", price: 500, image: "products/C5_Consoles/sony/ps5+controller.png", categoryId: 5,
+PNote: "825GB, Comes with a controller"
+, Prate: "★★★★★"};
+let p18={Pid: 1800, Pname: "Controller for PlayStation 5", price: 70, image: "products/C5_Consoles/sony/ps5controller.png", categoryId: 5,
+PNote: "Color : White"
+, Prate: "★★★★★"};
+let p19={Pid: 1900, Pname: "PlayStation 4 Pro", price: 250, image: "products/C5_Consoles/sony/PS4Pro.png", categoryId: 5,
+PNote: "1TB, Comes with a controller"
+, Prate: "★★★★☆"};
+let p20={Pid: 2000, Pname: "Sony PlayStation 4 Slim", price: 200, image: "products/C5_Consoles/sony/ps4slim+controller.png", categoryId: 5,
+PNote: "1TB, Comes with a controller"
+, Prate: "★★★★☆"};
+let p21={Pid: 2100, Pname: "Controller for PlayStation 4", price: 60, image: "products/C5_Consoles/sony/ps4controller.png", categoryId: 5,
+PNote: "Color : Black"
+, Prate: "★★★★☆"};
+let p22={Pid: 2200, Pname: "Sony PlayStation VR", price: 400, image: "products/C5_Consoles/sony/ps-vr.png", categoryId: 5,
+PNote: "Comes with a Sense controllers (L) And (R)"
+, Prate: "★★★★★"};
+let p23={Pid: 2300, Pname: "Xbox Series X", price: 500, image: "products/C5_Consoles/xbox/seriesX+Con.png", categoryId: 5,
+PNote: "1TB, Comes with a controller"
+, Prate: "★★★★★"};
+let p24={Pid: 2400, Pname: "Xbox Series S", price: 250, image: "products/C5_Consoles/xbox/seriesS.png", categoryId: 5,
+PNote: "512GB, Comes with a controller"
+, Prate: "★★★★☆"};
+let p25={Pid: 2500, Pname: "Xbox Series X / S Wireless", price: 60, image: "products/C5_Consoles/xbox/seriesXandSCon.png", categoryId: 5,
+PNote: "Color : Black"
+, Prate: "★★★★☆"};
+let p26={Pid: 2600, Pname: "Xbox One", price: 170, image: "products/C5_Consoles/xbox/XboxOne+Con.png", categoryId: 5,
+PNote: "512GB, Comes with a controller"
+, Prate: "★★★☆☆"};
+let p27={Pid: 2700, Pname: "Nintendo Switch OLED", price: 330, image: "products/C5_Consoles/nintendoSwitchOLED.png", categoryId: 5,
+PNote: "256GB, Comes with Black Joy-Con"
+, Prate: "★★★★☆"};
+let p28={Pid: 2800, Pname: "Meta Quest 2", price: 430, image: "products/C5_Consoles/oculusQuest2.png", categoryId: 5,
+PNote: "Meta Quest 2 is our most advanced all-in-one VR system yet"
+, Prate: "★★★★☆"}
+
+// C6 Games
+let p29={Pid: 2900, Pname: "Marvel's Spider-Man: Miles Morales", price: 40, image: "products/C6_games/SpiderMan.png", categoryId: 6,
+PNote: "Action-adventure | PEGI 16"
+, Prate: "★★★★★"};
+let p30={Pid: 3000, Pname: "Battlefield 2042", price: 50, image: "products/C6_games/Battlefield2042-50.png", categoryId: 6,
+PNote: "FP shooting | PEGI 16"
+, Prate: "★★★★☆"};
+let p31={Pid: 3100, Pname: "Mortal Kombat 11", price: 55, image: "products/C6_games/MortalKombat11-55.png", categoryId: 6,
+PNote: "Fighting | PEGI 12"
+, Prate: "★★★★☆"};
+let p32={Pid: 3200, Pname: "The Crew 2 ", price: 60, image: "products/C6_games/TheCrew2-60.png", categoryId: 6,
+PNote: "Open-World Racing | PEGI 12"
+, Prate: "★★★☆☆"};
+let p33={Pid: 3300, Pname: "god of war: ragnarok", price: 70, image: "products/C6_games/GodofWarRagnarok-70.png", categoryId: 6,
+PNote: "Action-adventure | PEGI 18"
+, Prate: "★★★★★"};
+let p34={Pid: 3400, Pname: "FAR CRY 6", price: 70, image: "products/C6_games/FARCRY6-70.png", categoryId: 6,
+PNote: "Action-adventure | PEGI 18"
+, Prate: "☆☆☆☆☆"};
+let p35={Pid: 3500, Pname: "FIFA 23", price: 80, image: "products/C6_games/FIFA23-80.png", categoryId: 6,
+PNote: "football game | PEGI 3"
+, Prate: "★★★★★"};
+let p36={Pid: 3600, Pname: "Watch Dogs: Legion", price: 80, image: "products/C6_games/WatchDogs_Legion-80.png", categoryId: 6,
+PNote: "Action-adventure | PEGI 18"
+, Prate: "★★★★☆"};
+
+
+// array of products
+var arrProd=[p37, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20,
+            p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p38, p39, p40];
+
+// Define the categories
+let c1= {Cid: 1, Cname: "Keyboard" }; // 8 products   1-4 && 37-40
+let c2= {Cid: 2, Cname: "Mouse"}; // 4 products   5-8
+let c3= {Cid: 3, Cname: "Headphones"}; // 4 products    9-12
+let c4= {Cid: 4, Cname: "Chairs"}; // 4 products    13-16
+let c5= {Cid: 5, Cname: "Consoles"}; // 12 products   17-28
+let c6= {Cid: 6, Cname: "games"}; // 8 products   29-36
+
+var arrCat=[c1, c2, c3, c4, c5, c6]; // array of categories
+
+console.log("JS work!"); // Test
+document.addEventListener("DOMContentLoaded", function() {
+  // When the DOM content is loaded, execute the following code
+  
+  // Get the categories list container
+  var categoriesList = document.querySelector(".categories ul");
+  
+  // Loop through each category in the arrCat array
+  arrCat.forEach(function(category) {
+    // Create a new list item for the category
+    var categoryItem = document.createElement("li");
+    // Set the text content of the list item to the category name
+    categoryItem.textContent = category.Cname;
+
+    // Generate a random category ID
+    var randomCategoryId = arrCat[Math.floor(Math.random() * arrCat.length)].Cid;
+    
+    // Call the showProductsByCategory function with a random category ID
+    showProductsByCategory(randomCategoryId); 
+    
+    // Add a click event listener to the category item
+    categoryItem.addEventListener("click", function() {
+      // Call the showProductsByCategory function with the clicked category ID
+      showProductsByCategory(category.Cid);
+    });
+    
+    // Append the category item to the categories list
+    categoriesList.appendChild(categoryItem);
+  });
+
+  // Function to display products based on a category ID
+  function showProductsByCategory(categoryId) {
+    // Get the products container element
+    var productsContainer = document.querySelector(".products");
+    // Clear the existing content of the products container
+    productsContainer.innerHTML = "";
+
+    // Loop through each product in the arrProd array
+    arrProd.forEach(function(product) {
+      // Check if the product belongs to the specified category
+      if (product.categoryId === categoryId) { 
+        // Create a new product div element
+        var productDiv = document.createElement("div");
+        productDiv.classList.add("product");
+
+        // Create an image element for the product
+        var productImage = document.createElement("img");
+        productImage.src = product.image;
+        productImage.alt = product.Pname;
+        productDiv.appendChild(productImage);
+
+        // Create a heading element for the product name
+        var productName = document.createElement("h3");
+        productName.textContent = product.Pname;
+        productDiv.appendChild(productName);
+
+        // Create a paragraph element for the product rate
+        var productInfo = document.createElement("p");
+        productInfo.textContent = "Rate: " + product.Prate;
+        productDiv.appendChild(productInfo);
+
+        // Create a paragraph element for the product description
+        var productInfo = document.createElement("p");
+        productInfo.textContent = "Description: " + product.PNote;
+        productDiv.appendChild(productInfo);
+
+        // Create a paragraph element for the product price
+        var productPrice = document.createElement("p");
+        productPrice.textContent = "Price: $" + product.price;
+        productDiv.appendChild(productPrice);
+
+        // Create a button to add the product to the cart
+        var addToCartBtn = document.createElement("button");
+        addToCartBtn.textContent = "Add to Cart"; 
+        addToCartBtn.addEventListener("click", function() {
+          // Call the addToCart function with the clicked product
+          addToCart(product); 
+        });
+        productDiv.appendChild(addToCartBtn);
+
+        // Append the product div to the products container
+        productsContainer.appendChild(productDiv);
+      }
+    });
+  }
+
+  // Array to store cart items
+  var cart = []; 
+
+  // Function to add a product to the cart
+  function addToCart(product) {
+    // Create a cart item object with product details
+    var cartItem = {
+      productId: product.Pid,
+      name: product.Pname,
+      price: product.price,
+      quantity: 1,
+      image: product.image
+    }; 
+
+    // Check if the cart item already exists in the cart
+    var existingCartItem = cart.find(function(item) {
+      return item.productId === cartItem.productId;
+    });
+
+    // If the cart item already exists, increase the quantity
+    if (existingCartItem) {
+      existingCartItem.quantity++;
+    } else {
+      // Otherwise, add the cart item to the cart array
+      cart.push(cartItem);
+    }
+
+    // Update the cart display
+    updateCart();
+  }
+
+  // Function to remove a cart item based on the product ID
+  function removeCartItem(productId) {
+    // Filter out the cart item with the specified product ID
+    cart = cart.filter(function(item) {
+      return item.productId !== productId;
+    });
+
+    // Update the cart display
+    updateCart();
+  }
+
+  // Function to update the quantity of a cart item
+  function updateQuantity(productId, newQuantity) {
+    // Find the cart item with the specified product ID
+    var cartItem = cart.find(function(item) {
+      return item.productId === productId;
+    });
+
+    // If the cart item is found, update its quantity and update the cart display
+    if (cartItem) {
+      cartItem.quantity = newQuantity;
+      updateCart();
+    }
+  }
+
+  // Function to update the cart display
+  function updateCart() { 
+    // Get the cart items container element
+    var cartItemsContainer = document.getElementById("cart-items");
+    // Clear the existing content of the cart items container
+    cartItemsContainer.innerHTML = "";
+    var total = 0;
+
+    // Loop through each item in the cart array
+    cart.forEach(function(item) {
+      // Create a new table row for the cart item
+      var cartItemRow = document.createElement("tr"); 
+
+      // Create a table cell for the item image
+      var imageCell = document.createElement("td"); 
+      var image = document.createElement("img");
+      image.src = item.image;
+      image.alt = item.name;
+      image.width = 50;
+      imageCell.appendChild(image);
+      cartItemRow.appendChild(imageCell);
+
+      // Create a table cell for the item name
+      var productNameCell = document.createElement("td");
+      productNameCell.textContent = item.name;
+      cartItemRow.appendChild(productNameCell);
+
+      // Create a table cell for the item price
+      var priceCell = document.createElement("td");
+      priceCell.textContent = "$" + item.price;
+      cartItemRow.appendChild(priceCell); 
+
+      // Create a table cell for the item quantity
+      var quantityCell = document.createElement("td");
+      var quantityInput = document.createElement("input");
+      quantityInput.type = "number";
+      quantityInput.min = "1";
+      quantityInput.value = item.quantity;
+      quantityInput.addEventListener("input", function() {
+        // Call the updateQuantity function with the updated quantity value
+        updateQuantity(item.productId, parseInt(quantityInput.value));
+      });
+      quantityCell.appendChild(quantityInput);
+      cartItemRow.appendChild(quantityCell);
+
+      // Create a table cell for the item total
+      var totalCell = document.createElement("td");
+      var itemTotal = item.price * item.quantity; 
+      totalCell.textContent = "$" + itemTotal;
+      total += itemTotal;
+      cartItemRow.appendChild(totalCell); 
+
+      // Create a table cell for the remove button
+      var removeButtonCell = document.createElement("td");
+      var removeButton = document.createElement("button");
+      removeButton.textContent = "Remove";
+      removeButton.addEventListener("click", function() { 
+        // Call the removeCartItem function with the product ID to remove
+        removeCartItem(item.productId); 
+      }); 
+      removeButtonCell.appendChild(removeButton);
+      cartItemRow.appendChild(removeButtonCell);
+
+      // Append the cart item row to the cart items container
+      cartItemsContainer.appendChild(cartItemRow);
+    });
+
+    // Update the cart total
+    var cartTotal = document.getElementById("cart-total");
+    cartTotal.textContent = "Total: $" + total; 
+  }
+
+  // Function to handle the checkout process
+  function checkout() {
+    var total = 0;
+    
+    // Calculate the total amount to be paid by summing up item totals in the cart
+    cart.forEach(function(item) {
+      var itemTotal = item.price * item.quantity;
+      total += itemTotal;
+    }); 
+    
+    // Clear the cart and update the cart display
+    cart = [];
+    updateCart();
+    
+    // Display a thank you message or alert if there are no products in the cart
+    if (total == 0) {
+      var addprod = "Please add products to your cart";
+      alert(addprod);
+    } else {
+      var thankYouMessage = "Thank you for the purchase. Total to be paid: $" + total;
+      alert(thankYouMessage);
+    }
+  }
+    
+  // Add a click event listener to the checkout button
+  var checkoutButton = document.getElementById("checkout-btn");
+  checkoutButton.addEventListener("click", checkout);
+
+});
